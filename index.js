593a342ef4f4b961e5e34a20e8f7bd7fce9e42d9
Aashu Sharma <buildforfb@gmail.com>
2023-07-23 00:19:28 +0530

    request entity size increased to 30mb

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const io = new socketServer(server, { cors: { origin: "*" } });
 // });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 const rooms = {
